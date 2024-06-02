714605fd16932b3330a6de2a8722761d23717b06
jizhuoran <jizr@connect.hku.hk>
2024-06-02 18:59:02 +0800

    clean code

--- py_plonk/plonk_core/src/proof_system/permutation.py
+++ py_plonk/plonk_core/src/proof_system/permutation.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from ....bls12_381 import fr
 from typing import List, Tuple
 from ....domain import Radix2EvaluationDomain
-from ....arithmetic import poly_mul_const,poly_add_poly,from_gmpy_list_1,neg
+from ....arithmetic import poly_mul_const,poly_add_poly,from_gmpy_list_1
 from ....plonk_core.src.permutation.constants import K1,K2,K3
 import torch.nn.functional as F
 import torch
